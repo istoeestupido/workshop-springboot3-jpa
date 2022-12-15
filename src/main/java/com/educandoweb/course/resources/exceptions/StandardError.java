f23e50f18356a57385b140ebb488e98a449f746b
Joana Silva <joana.joanasantossilva.silva@gmail.com>
2022-12-15 12:39:35 +0000

    Exception handling - findById

---
+++ src/main/java/com/educandoweb/course/resources/exceptions/StandardError.java
@@ -0,0 +1,69 @@
+package com.educandoweb.course.resources.exceptions;
+
+import java.io.Serializable;
+import java.time.Instant;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+
+public class StandardError implements Serializable{
+	private static final long serialVersionUID = 1L;
+	
+	@JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyy-MM-dd'T'HH:mm:ss'Z'", timezone = "GMT")
+	private Instant timeStamp;
+	private Integer id;
+	private String error;
+	private String message;
+	private String path;
+
+	public StandardError() {
+	}
+
+	public StandardError(Instant timeStamp, Integer id, String error, String message, String path) {
+		this.timeStamp = timeStamp;
+		this.id = id;
+		this.error = error;
+		this.message = message;
+		this.path = path;
+	}
+
+	public Instant getTimeStamp() {
+		return timeStamp;
+	}
+
+	public void setTimeStamp(Instant timeStamp) {
+		this.timeStamp = timeStamp;
+	}
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getError() {
+		return error;
+	}
+
+	public void setError(String error) {
+		this.error = error;
+	}
+
+	public String getMessage() {
+		return message;
+	}
+
+	public void setMessage(String message) {
+		this.message = message;
+	}
+
+	public String getPath() {
+		return path;
+	}
+
+	public void setPath(String path) {
+		this.path = path;
+	}
+
+}
